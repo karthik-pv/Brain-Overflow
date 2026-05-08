5d1886689019fcecbe6d1b0750967f0ebffb2cb2
BipinRajC <bipinraj.4604@gmail.com>
2026-05-09 03:22:24 +0530

    feat: initialize Hive boxes and register PendingIdea adapter in main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'models/pending_idea.dart';
 import 'router.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(PendingIdeaAdapter());
+  await Hive.openBox('credentials');
+  await Hive.openBox('room');
+  await Hive.openBox<PendingIdea>('pending_ideas');
+
   runApp(
     const ProviderScope(
       child: BrainOverflowApp(),
